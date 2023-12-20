e39047bf6fb4d984054d825023fce4a2337b56aa
blazarcosmos <142469595+blazarcosmos@users.noreply.github.com>
2023-12-19 22:55:03 -0600

    Revert "se agregó una app Express.js"
    This reverts commit 4d680c97c75d47944184fcffffb84ecd3f146707.

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const path = require('path');
-const port = 3000;
-
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'mi-repositorio')));
-
-
-app.listen(port, () => {
-    console.log("hola mundo");
-});
+console.log("hola mundo");
