2c207a1f956efdcd2d6f85ea9fd1b3c12aecdec7
blazarcosmos <142469595+blazarcosmos@users.noreply.github.com>
2023-12-19 23:02:09 -0600

    se agrego nuevamente una app Epress.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("hola mundo");
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'mi-repositorio')));
+
+app.listen(port, () => {
+    console.log(`localhost:${port}`);
+});
